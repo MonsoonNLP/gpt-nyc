d12e27d76da98b9b2870e0b7add536060e30629f
Nick Doiron <ndoiron@mapmeld.com>
2021-06-15 23:25:36 -0600

    gpt-nyc landing page

---
+++ app.js
@@ -0,0 +1,9 @@
+function aboutme(e) {
+  e.target.style.display = "none";
+  document.querySelector("#aboutme").style.display = "block";
+}
+
+function aboutlangs(e) {
+  e.target.style.display = "none";
+  document.querySelector("#aboutlangs").style.display = "block";
+}
